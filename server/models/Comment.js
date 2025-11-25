680b13761e1ad4cc6d84ec5055ed9b571297c48d
Prudence Oyando <prudenceoyando98@gmail.com>
2025-11-25 10:20:32 +0300

    Create Comment model with schema and timestamps

---
+++ server/models/Comment.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+  authorName: { type: String, required: true },
+  content: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Comment', commentSchema);
